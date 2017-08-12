bb9b1fea70e5de280093ff4b7f98cb89166164c2
lisaula <luisca_elfather@hotmail.com>
2017-08-11 22:30:56 -0600

    inputs and buttono

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Prosoft Calendar</h2>
+            <input className='input' type='text' className='Country' placeholder='US' onChange={this.handleInputChange}/>
+            <input className='input' type='text' className='StartDate' placeholder='YYYY-MM-DD' onChange={this.handleInputChange}/>
+            <input className='input' type='text' className='Days' placeholder='30' onChange={this.handleInputChange}/>
+            <button onClick={this.handleButton}>Go</button>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
